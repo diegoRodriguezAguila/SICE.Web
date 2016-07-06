cbaba49df966e6fa963722cc98c5a200395cbde5
diegoRodriguezAguila <diroag@gmail.com>
2016-07-06 10:57:31 -0400

    Added Companies entity, in order to perform mobile authentication

---
+++ config/Migrations/20160706144658_CreateCompanies.php
@@ -0,0 +1,45 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateCompanies extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('companies');
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('username', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('registration_token', 'string', [
+            'default' => null,
+            'limit' => 512,
+            'null' => false,
+        ]);
+        $table->addColumn('status', 'integer', [
+            'default' => 1,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
